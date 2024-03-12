0f764572412ea0e2b0da47373d9268a54132d91b
werapol <r5r5wa55@hotmail.com>
2024-03-12 12:47:16 +0700

    adduser

--- src/page/Crud.jsx
+++ src/page/Crud.jsx
@@ -6,7 +6,13 @@ export default function Crud() {
 
     const [datafech,setDataFech]=useState([])
     const [model,setmodel]=useState(false)
-    console.log(model);
+
+    const [fname,setFname]=useState()
+    const [lname,setLname]=useState()
+    const [email,setEmail]=useState()
+    const [password,setPassword]=useState()
+
+
     useEffect(()=>{
     const fetchDb=async()=>{
 
@@ -19,15 +25,24 @@ export default function Crud() {
         }   
     }
     fetchDb()
-    },[])
+    },[datafech])
+
+    const hadleSubmit=async(e)=>{
+        e.preventDefault()
+         await axios.post(`http://localhost:8000/api/create`,{fname,lname,email,password})
+        .then(res=>{console.log(res)})
+        .catch(err=>console.log(err))
+        setmodel(false)
+    }
+   
+    
   return (
    <div >
-        <div className="sm:max-w-4xl mx-auto mt-28" >
+        <div className="sm:max-w-4xl mx-auto mt-20" >
             <button className="p-2 bg-fuchsia-600 rounded-lg text-white font-bold" onClick={()=>setmodel(!model)} >Add User</button>
         </div>
-        
-
-        <div className="max-w-5xl mx-auto  bg-neutral-50 shadow-xl " >
+    
+        <div className="max-w-5xl mx-auto mb-20 bg-neutral-50 shadow-xl " >
         
         <div className="w-full">
             <button></button>
@@ -48,10 +63,10 @@ export default function Crud() {
                             <li>{data?.email}</li>
                             <li>
                                 <button className="p-2 bg-red-700 rounded-lg  text-white mx-2">
-                                    <MdDeleteForever className="text-2xl"/>
+                                    <MdDeleteForever className="sm:text-2xl"/>
                                 </button>
                                 <button className="p-2 bg-green-500 rounded-lg  text-white">
-                                    <MdCreditScore className="text-2xl" />
+                                    <MdCreditScore className="sm:text-2xl" />
                                 </button>
                             </li>
                         </ul>
@@ -63,69 +78,90 @@ export default function Crud() {
             </div>
         </div>
         </div>
+        
+        
         {
             model &&
             
-
-
-        <>
-            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                {/*content*/}
-                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                    {/*header*/}
-                    <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                    <h3 className="text-3xl font-semibold">
-                        Add User
-                    </h3>
-             
+            <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col md:w-[700px] bg-white outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                        <h3 className="text-3xl font-semibold">
+                            Add User
+                        </h3>
+                
+                        </div>
+                        {/*body*/}
+                        <form onSubmit={hadleSubmit}>
+                            <div className=" p-6 flex-auto w-2xl">
+                                <div className="flex flex-col gap-5 p-5">
+                                    <input 
+                                        required
+                                        type="input" 
+                                        placeholder="Name" 
+                                        className="p-2 border-b-2 w-full border-violet-950 outline-none text-xl mb-2"  
+                                        id="fname"
+                                        name="fname"
+                                        onChange={(e)=>setFname(e.target.value)}
+                                    />
+                                    <input 
+                                        required
+                                        type="text" 
+                                        placeholder="Lastname"
+                                        className="p-2 border-b-2 w-full border-violet-950 outline-none text-xl mb-2"  
+                                        id="lname"
+                                        name="lname"
+                                         onChange={(e)=>setLname(e.target.value)}
+                                    />
+                                    <input 
+                                        required
+                                        type="text" 
+                                        placeholder="Email"
+                                        className="p-2 border-b-2 w-full border-violet-950 outline-none text-xl mb-2"  
+                                        id="email"
+                                        name="email"
+                                         onChange={(e)=>setEmail(e.target.value)}
+                                    />
+                                    <input 
+                                        required
+                                        type="text" 
+                                        placeholder="Password" 
+                                        className="p-2 border-b-2 w-full border-violet-950 outline-none text-xl mb-2" 
+                                        id="password"
+                                        name="password"
+                                         onChange={(e)=>setPassword(e.target.value)}
+                                    />
+                                </div>
+                        
+                            </div>                     
+                            {/*footer*/}
+                            <div className="flex items-center justify-end p-6 rounded-b">
+                                <button
+                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={() => setmodel(false)}
+                                >
+                                    Close
+                                </button>
+                                <button
+                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="submit"
+                                
+                                >
+                                    Save Changes
+                                </button>
+                            </div>
+                        </form>
+            
                     </div>
-                    {/*body*/}
-                    <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                        I always felt like I could do anything. That’s the main
-                        thing people are controlled by! Thoughts- their perception
-                        of themselves! slowed down by their perception of
-                        themselves. taught you can’t do anything, you
-                        won’t do anything. I was taught I could do everything.
-                    </p>
-                    </div>
-                    {/*footer*/}
-                    <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                    <button
-                        className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        onClick={() => setmodel(false)}
-                    >
-                        Close
-                    </button>
-                    <button
-                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                        type="button"
-                        onClick={() => setmodel(false)}
-                    >
-                        Save Changes
-                    </button>
                     </div>
                 </div>
-                </div>
-            </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+              
+            </>
 
         }
    </div>

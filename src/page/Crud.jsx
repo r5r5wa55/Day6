d8c92dab7360c66292a28cac454dbf9932c24167
werapol <r5r5wa55@hotmail.com>
2024-03-12 19:20:57 +0700

    delete

--- src/page/Crud.jsx
+++ src/page/Crud.jsx
@@ -4,35 +4,82 @@ import { MdCreditScore ,MdDeleteForever} from "react-icons/md";
 
 export default function Crud() {
 
-    const [datafech,setDataFech]=useState([])
+
     const [model,setmodel]=useState(false)
+    const [modeldelete,setmodelDelete]=useState(false)
+    const [fname,setFname]=useState('')
+    const [lname,setLname]=useState('')
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
 
-    const [fname,setFname]=useState()
-    const [lname,setLname]=useState()
-    const [email,setEmail]=useState()
-    const [password,setPassword]=useState()
+    const [loading,setLoading]=useState(true)
+    const [datafech,setDataFech]=useState([])
+    // const [users,setUsers]=useState(user)
+    const [deletid,setDeletId]=useState('')
 
 
+
+    const user={
+        fname:fname,
+        lname:lname,
+        email:email,
+        password:password
+    }
+
+    // console.log(user);
+    
     useEffect(()=>{
     const fetchDb=async()=>{
-
+        setLoading(false)
         try {
+          
             const res = await axios.get(`http://localhost:8000/api/getall`)
             const data  = await res.data
             setDataFech(data)
+        
+      
         } catch (error) {
             console.log(error);
         }   
+        setLoading(true)
     }
     fetchDb()
-    },[datafech])
+    },[])
+
+
+
+
 
     const hadleSubmit=async(e)=>{
+     
         e.preventDefault()
-         await axios.post(`http://localhost:8000/api/create`,{fname,lname,email,password})
-        .then(res=>{console.log(res)})
+        await axios.post(`http://localhost:8000/api/create`,user)
+        .then(res=>{
+            console.log(res);
+            window.location.reload();
+        })
         .catch(err=>console.log(err))
         setmodel(false)
+  
+    
+    }
+    const DeleteUser=(e)=>{
+        const id = e
+        setDeletId(id)
+       
+        setmodelDelete(true)
+    }
+    const DeletIdUser=async()=>{
+        await axios.delete(`http://localhost:8000/api/deleteuser/${deletid}`)
+        .then((res)=>{
+            setDataFech((pervUser)=>pervUser.filter((user)=>user._id !== deletid))
+            console.log(res);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        console.log(deletid);
+        setmodelDelete(false)
     }
    
     
@@ -53,35 +100,41 @@ export default function Crud() {
                     <li>User Email</li>
                     <li>Action</li>
                 </ul>
-                {datafech &&
-                    datafech?.map((data)=>(
-
-               
-                        <ul className="w-full grid  grid-cols-4 p-5 text-center hover:bg-slate-200 h-full  items-center" key={data?._id}>
-                            <li>{data?.lname}</li>
-                            <li>{data?.lname} {data?.fname}</li>
-                            <li>{data?.email}</li>
-                            <li>
-                                <button className="p-2 bg-red-700 rounded-lg  text-white mx-2">
-                                    <MdDeleteForever className="sm:text-2xl"/>
-                                </button>
-                                <button className="p-2 bg-green-500 rounded-lg  text-white">
-                                    <MdCreditScore className="sm:text-2xl" />
-                                </button>
-                            </li>
-                        </ul>
-           
-
-                    ))
-                    
-                }
+       
+             
+                   {loading ? 
+                        <div className="">
+                            {datafech &&
+                            datafech?.map((data,index)=>(               
+                                <ul className="w-full grid  grid-cols-4 p-5 text-center hover:bg-slate-200 h-full  items-center" key={index}>
+                                    <li>{index+1}</li>
+                                    <li>{data?.lname} {data?.fname}</li>
+                                    <li>{data?.email}</li>
+                                    <li>
+                                        <button 
+                                            className="p-2 bg-red-700 rounded-lg  text-white mx-2" 
+                                            onClick={()=>DeleteUser(data._id)}
+                                        >
+                                            <MdDeleteForever className="sm:text-2xl"/>
+                                        </button>
+                                        <button className="p-2 bg-green-500 rounded-lg  text-white">
+                                            <MdCreditScore className="sm:text-2xl" />
+                                        </button>
+                                    </li>
+                                </ul>
+                            ))
+                            
+                            }
+                        </div>
+                        :
+                        <div className="text-3xl h-[600px] flex justify-center items-center" >loading</div>
+                   }
             </div>
         </div>
         </div>
         
         
-        {
-            model &&
+        {model &&
             
             <>
                 <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -163,6 +216,52 @@ export default function Crud() {
               
             </>
 
+        }
+
+        { modeldelete &&
+            
+            <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col md:w-[500px] bg-white outline-none focus:outline-none">
+                        {/*header*/}
+                        <div className="flex items-start justify-between p-5  rounded-t">
+                        <h3 className="text-3xl font-semibold text-center w-full">
+                            Delete User
+                        </h3>
+                
+                        </div>
+                        {/*body*/}
+                    
+                            <div className="  flex-auto w-2xl w-5/6 mx-auto text-center bg-red-300 p-2 rounded-md">
+                                confirm delete
+                        
+                            </div>                     
+                            {/*footer*/}
+                            <div className="flex items-center justify-end p-6 rounded-b">
+                                <button
+                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={() => setmodelDelete(false)}
+                                >
+                                    Close
+                                </button>
+                                <button
+                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    onClick={DeletIdUser}
+                                    
+                                >
+                                     confirm 
+                                </button>
+                            </div>
+                 
+            
+                    </div>
+                    </div>
+                </div>
+              
+            </>
         }
    </div>
    
